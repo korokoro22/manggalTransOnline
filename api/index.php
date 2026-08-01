ef296fc0053ccd58057937524270a8f4ee3a429c
korokoro22 <andimuhammadrezkym@gmail.com>
2026-08-01 16:25:14 +0800

    fixing failed to display web on vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,8 @@
 <?php
-require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require __DIR__ . '/../bootstrap/app.php';
+
+return $app->handle(
+    Illuminate\Http\Request::capture()
+);
