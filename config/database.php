2f34efcf4908a37b122b1be3b7187f86563f546f
korokoro22 <andimuhammadrezkym@gmail.com>
2026-08-01 14:50:15 +0800

    deleting LaporanBus.php (model)

--- config/database.php
+++ config/database.php
@@ -60,9 +60,9 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', base_path('cacert.pem')),
             ]) : [],
-        ],
+                    ],
 
         'mariadb' => [
             'driver' => 'mariadb',
